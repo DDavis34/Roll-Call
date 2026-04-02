03c9f53aa74e4c3ea5036bc4ad3acb11df27c7fe
MichaelRdot <mruss62@lsu.edu>
2026-04-02 10:16:01 -0500

    Ancestry added and two new Models added to TODO list

--- PF2EGM/Models/Ancestry.cs
+++ PF2EGM/Models/Ancestry.cs
@@ -2,5 +2,17 @@
 
 public class Ancestry
 {
-    
+    public string Name { get; set; }
+    public string Source { get; set; }
+    public string Rarity { get; set; }
+    public string Pfs { get; set; }
+    public string Size { get; set; }
+    public string Speed { get; set; }
+    public int HitPointsBase { get; set; }
+    public string Vision { get; set; }
+    public List<string> Language { get; set; }
+    public List<Attribute> AttributeFlaw { get; set; }
+    public List<Attribute> AttributeBoost { get; set; }
+    public List<Heritage> Heritages { get; set; }
+    public List<Ability> Abilities { get; set; }
 }
