3041d29937d252980797a35b16b9247bcb2e7c49
DDavis34 <ddav191@lsu.edu>
2026-04-02 12:11:23 -0500

    JsonPropertyName

--- PF2EGM/Models/Ancestry.cs
+++ PF2EGM/Models/Ancestry.cs
@@ -1,6 +1,37 @@
-﻿namespace PF2EGM.Models;
+﻿using System.Text.Json.Serialization;
 
-public class Ancestry
+namespace PF2EGM.Models
 {
-    
+    public class Ancestry
+    {
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("hp")]
+        public required string Hp { get; set; }
+
+        [JsonPropertyName("size")]
+        public required string Size { get; set; }
+
+        [JsonPropertyName("speed")]
+        public required string Speed { get; set; }
+
+        [JsonPropertyName("ability_boost")]
+        public required string AbilityBoost { get; set; }
+
+        [JsonPropertyName("ability_flaw")]
+        public required string AbilityFlaw { get; set; }
+
+        [JsonPropertyName("language")]
+        public required string Language { get; set; }
+
+        [JsonPropertyName("vision")]
+        public required string Vision { get; set; }
+
+        [JsonPropertyName("rarity")]
+        public required string Rarity { get; set; }
+
+        [JsonPropertyName("pfs")]
+        public required string Pfs { get; set; }
+    }
 }
