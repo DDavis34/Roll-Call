36fb491f4eb3dca1816c0d072e72ca6607607942
DDavis34 <ddav191@lsu.edu>
2026-04-09 14:55:58 -0500

    Completed majority of models

--- PF2EGM/Models/Feat.cs
+++ PF2EGM/Models/Feat.cs
@@ -1,6 +1,31 @@
-﻿namespace PF2EGM.Models;
+﻿using System.Text.Json.Serialization;
 
-public class Feat
+namespace PF2EGM.Models
 {
-    
+    public class Feat
+    {
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("level")]
+        public required string Level { get; set; }
+
+        [JsonPropertyName("trait")]
+        public required string Trait { get; set; }
+
+        [JsonPropertyName("prerequisite")]
+        public required string Prerequisite { get; set; }
+
+        [JsonPropertyName("summary")]
+        public required string Summary { get; set; }
+
+        [JsonPropertyName("rarity")]
+        public required string Rarity { get; set; }
+
+        [JsonPropertyName("pfs")]
+        public required string Pfs { get; set; }
+
+        [JsonPropertyName("source")]
+        public required string Source { get; set; }
+    }
 }
--- PF2EGM/Models/Skill.cs
+++ PF2EGM/Models/Skill.cs
@@ -1,6 +1,34 @@
-﻿namespace PF2EGM.Models;
+﻿using System.Text.Json.Serialization;
 
-public class Skill
+namespace PF2EGM.Models
 {
-    
+    public class Skill
+    {
+        [JsonPropertyName("name")]
+        public required string Name { get; set; }
+
+        [JsonPropertyName("pfs")]
+        public required string Pfs { get; set; }
+
+        [JsonPropertyName("source")]
+        public required string Source { get; set; }
+
+        [JsonPropertyName("rarity")]
+        public required string Rarity { get; set; }
+
+        [JsonPropertyName("trait")]
+        public required string Trait { get; set; }
+
+        [JsonPropertyName("level")]
+        public required string Level { get; set; }
+
+        [JsonPropertyName("prerequisite")]
+        public required string Prerequisite { get; set; }
+
+        [JsonPropertyName("summary")]
+        public required string Summary { get; set; }
+
+        [JsonPropertyName("spoilers")]
+        public required string Spoilers { get; set; }
+    }
 }
