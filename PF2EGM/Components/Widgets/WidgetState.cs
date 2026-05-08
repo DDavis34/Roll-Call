5f1b8b5403036957da94ac9d9b11a0de6f1f480e
JosiahMatriarch <corbinbrescher@gmail.com>
2026-05-07 22:55:50 -0500

    finished up widget system, will add "add widget" button

---
+++ PF2EGM/Components/Widgets/WidgetState.cs
@@ -0,0 +1,10 @@
+namespace PF2EGM.Components.Widgets;
+
+public class WidgetState
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Width { get; set; }
+    public bool Visible { get; set; } = true;
+    public bool Minimized { get; set; }
+}
